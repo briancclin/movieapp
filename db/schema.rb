bdec2ed68f519a8a576748cf0cedc8c662c58615
briancclin <briancclin@gmail.com>
2014-08-25 11:58:24 -0700

    psql update

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140823014153) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: true do |t|
     t.string   "name"
     t.integer  "year"
