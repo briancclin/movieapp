89c140bab9b6d86a690a792cf79acebc041c1145
briancclin <briancclin@gmail.com>
2014-08-31 22:30:14 -0700

    require rotten

--- add_rotten_info.rb
+++ add_rotten_info.rb
@@ -4,20 +4,22 @@ Rotten.api_key = "twvtkhmbbdkys45fk9tudk5t"
 
 movie = Movie.all
 movie.each do |item|
-  rotten = RottenMovie.find(:id => item.rotten_id)
-  if rotten.empty?
-    item.update(:rotten_link => "")
-  else
-    item.update(:rotten_link => rotten.links.alternate)
-  end
-  if rotten.empty?
-    fresh = false
-  else
-    if rotten.ratings.critics_rating == "Certified Fresh"
-      fresh = true
+  if item.rotten_id != -1
+    rotten = RottenMovie.find(:id => item.rotten_id)
+    if rotten.empty?
+      item.update(:rotten_link => "")
     else
-      fresh = false
+      item.update(:rotten_link => rotten.links.alternate)
     end
+    if rotten.empty?
+      fresh = false
+    else
+      if rotten.ratings.critics_rating == "Certified Fresh"
+        fresh = true
+      else
+        fresh = false
+      end
+    end
+    item.update(:certified_fresh => fresh)
   end
-  item.update(:certified_fresh => fresh)
 end
