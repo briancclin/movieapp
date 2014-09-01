a9c2f42bf804671ec70e41216e5a370a877c69a9
briancclin <briancclin@gmail.com>
2014-08-31 23:00:37 -0700

    Sort changes

--- add_rotten_info.rb
+++ add_rotten_info.rb
@@ -4,22 +4,20 @@ Rotten.api_key = "twvtkhmbbdkys45fk9tudk5t"
 
 movie = Movie.all
 movie.each do |item|
-  if item.rotten_id != -1
-    rotten = RottenMovie.find(:id => item.rotten_id)
-    if rotten.empty?
-      item.update(:rotten_link => "")
-    else
-      item.update(:rotten_link => rotten.links.alternate)
-    end
-    if rotten.empty?
-      fresh = false
-    else
-      if rotten.ratings.critics_rating == "Certified Fresh"
-        fresh = true
-      else
-        fresh = false
-      end
-    end
-    item.update(:certified_fresh => fresh)
+  rotten = RottenMovie.find(:id => item.rotten_id, :limit => 1)
+  if rotten.empty?
+    item.update(:rotten_link => "")
+  else
+    item.update(:rotten_link => rotten.links.alternate)
   end
+  if rotten.empty?
+    fresh = false
+  else
+    if rotten.ratings.critics_rating == "Certified Fresh"
+      fresh = true
+    else
+      fresh = false
+    end
+  end
+  item.update(:certified_fresh => fresh)
 end
--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -3,7 +3,7 @@ require 'jquery-lazy-images'
 class MovieController < ApplicationController
  autocomplete :movie, :name, :full => true, :extra_data => [:year], :display_value => :movie_search_result
   def index
-    @movie = Movie.all
+    @movie = Movie.all.sort()
   end
   def show
     @movie = Tmdb::Movie.detail(params[:id])
