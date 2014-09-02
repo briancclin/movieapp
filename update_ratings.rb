9d3f70b327235d4604e6ba550d030ee9a4997347
briancclin <briancclin@gmail.com>
2014-09-01 19:14:06 -0700

    update_ratings

--- update_ratings.rb
+++ update_ratings.rb
@@ -6,17 +6,11 @@ Tmdb::Api.key("4ec3a19d6af74f88a492405690f72298")
 movie = Movie.find_by_sql("SELECT * FROM movies WHERE rotten_rating = '-1'")
 movie.each do |item|
    movie_rotten = RottenMovie.find(:title => item.name, :limit => 1)
-   if movie_rotten.nil? or movie_rotten.empty?
-    rotten_id = -1;
-    rotten_rating = -1;
-  else
-    rotten_id = movie_rotten.id 
-    if movie_rotten.ratings.nil? or movie_rotten.ratings.empty?
-      rotten_rating = -1;
-    else
-      rotten_rating = movie_rotten.ratings.critics_score
-    end
-  end
-  item.update(rotten_id: rotten_id)
+   if !movie_rotten.empty?
+     rotten_id = movie_rotten.id 
+     rotten_rating = movie_rotten.ratings.critics_score
+     item.update(rotten_id: rotten_id)
   item.update(rotten_rating: rotten_rating)
+   end
+   
 end
